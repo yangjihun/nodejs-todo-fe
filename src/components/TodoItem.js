765e39ff8f356b96c3c14790d3c3457abfd64fc0
jihun <yjhn0410@gmail.com>
2025-07-25 14:53:00 +0900

    complete 수정

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,12 +5,12 @@ const TodoItem = ({item, deleteTasks, updateTask }) => {
   return (
     <Row>
       <Col xs={12}>
-        <div className={item.isComplete?`todo-item`:`item-complete`}>
+        <div className={!item.isComplete?`todo-item`:`item-complete`}>
           <div className={item.isComplete?"todo-content":"todo-content done-content"}>{item.task}</div>
 
           <div>
             <button className="button-delete" onClick={()=>{deleteTasks(item)}}>삭제</button>
-            <button className="button-delete" onClick={()=>{updateTask(item)}}>{item.isComplete?"완료":"미완료"}</button>
+            <button className="button-delete" onClick={()=>{updateTask(item)}}>{!item.isComplete?"완료":"미완료"}</button>
           </div>
         </div>
       </Col>
